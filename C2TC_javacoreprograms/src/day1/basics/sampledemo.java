755c5cb44c0d18d38861d21e19549da57852c27c
GayathriGurubaran <gayathrigurubaran5@gmail.com>
2025-07-21 16:26:58 +0530

    This is my eclipse entity and normal program

---
+++ C2TC_javacoreprograms/src/day1/basics/sampledemo.java
@@ -0,0 +1,29 @@
+package day1.basics;
+
+public class sampledemo {
+	
+	public static void main()
+	{
+		System.out.println("welcome to form main 1 ");
+	}
+	
+	public static void main(char c[])
+	{
+		System.out.println("Welcome form main 2");
+	}
+	
+	public static void main(String ar)
+	{
+		System.out.println("welcome form 3");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to eclipse ");
+		System.out.println("next line");
+
+	}
+	
+	
+	
+
+}
---
+++ C2TC_javacoreprograms/src/day1/entity/Customer.java
@@ -0,0 +1,31 @@
+package day1.entity;
+
+public class Customer {
+
+		public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+		private int cid;
+		private String cname;
+		private String city;
+		
+		
+
+	}
+
+
